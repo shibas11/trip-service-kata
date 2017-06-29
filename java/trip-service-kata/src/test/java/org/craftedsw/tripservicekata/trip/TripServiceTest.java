e1b13e1533f187c67e668ff15a254ea0055efac7
Jake <kc80.lee@samsung.com>
2017-06-29 13:10:49 +0900

    add file quiz.txt

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,11 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.Test;
+
 public class TripServiceTest {
+    @Test
+    public void nothing() {
+        System.out.println("ready to test");
+    }
 	
 }
