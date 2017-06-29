5109d9e5f227c0d8971a543864b22c48df70a4f3
Jake <kc80.lee@samsung.com>
2017-06-29 15:17:25 +0900

    1.8 should_return_friend_trips_when_users_are_friends 테스트케이스 작성 but 실패 => DAO를 호출하고 있기 때문에

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -17,6 +17,7 @@ public class TripServiceTest {
     public static final User REGISTERED_USER = new User();
     public static final User ANOTHER_FRIEND = new User();
     public static final Trip TO_BRAZIL = new Trip();
+    public static final Trip TO_LONDON = new Trip();
     private User loggedInUser;
     private TripService tripService;
 
@@ -42,6 +43,20 @@ public class TripServiceTest {
         List<Trip> friendTrips = tripService.getTripsByUser(friend);
 
         assertThat(friendTrips.size(), is(0));
+    }
+
+    @Test
+    public void should_return_friend_trips_when_users_are_friends() {
+        loggedInUser = REGISTERED_USER;
+
+        User friend = new User();
+        friend.addFriend(ANOTHER_FRIEND);
+        friend.addFriend(loggedInUser);
+        friend.addTrip(TO_BRAZIL);
+        friend.addTrip(TO_LONDON);
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
+
+        assertThat(friendTrips.size(), is(2));
     }
 
     private class TestableTripService extends TripService {
