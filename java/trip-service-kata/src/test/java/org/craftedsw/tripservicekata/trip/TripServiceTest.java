3485e6c218b22825ee8d86701c49b1b464079a98
Jake <kc80.lee@samsung.com>
2017-06-29 18:12:47 +0900

    1.10 중복 코드 제거 - move tripService init to setup()

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -24,6 +24,7 @@ public class TripServiceTest {
     @Before
     public void setUp() throws Exception {
         tripService = new TestableTripService();
+        loggedInUser = REGISTERED_USER;
     }
 
     @Test(expected = UserNotLoggedInException.class)
@@ -35,8 +36,6 @@ public class TripServiceTest {
 
     @Test
     public void should_not_return_any_trips_when_users_are_not_friends() {
-        loggedInUser = REGISTERED_USER;
-
         User friend = new User();
         friend.addFriend(ANOTHER_FRIEND);
         friend.addTrip(TO_BRAZIL);
@@ -47,8 +46,6 @@ public class TripServiceTest {
 
     @Test
     public void should_return_friend_trips_when_users_are_friends() {
-        loggedInUser = REGISTERED_USER;
-
         User friend = new User();
         friend.addFriend(ANOTHER_FRIEND);
         friend.addFriend(loggedInUser);
