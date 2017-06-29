9f743775af279dcc94ca5d62bb99a9f0f076d7a8
Jake <kc80.lee@samsung.com>
2017-06-29 14:30:00 +0900

    1.4 extract field - loggedInUser => 테스트 코드에 User 표현되게 함

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -5,9 +5,14 @@ import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
 public class TripServiceTest {
+
+    private User loggedInUser;
+
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged_in() {
         TripService tripService = new TestableTripService();
+
+        loggedInUser = null;
 
         tripService.getTripsByUser(null);
     }
@@ -15,7 +20,7 @@ public class TripServiceTest {
     private class TestableTripService extends TripService {
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedInUser;
         }
     }
 }
