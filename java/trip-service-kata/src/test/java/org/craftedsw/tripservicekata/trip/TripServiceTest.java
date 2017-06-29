27b4ce5e03e35f941ce792e36aba40de3c3ab967
Jake <kc80.lee@samsung.com>
2017-06-29 13:35:45 +0900

    1.3 add inner class - TestableTripService, override getLoggedUser and change(return null) => UserSession에서 발생하는 오류는 TripService의 영역이 아니므로, getLoggedUser를 override해서 null을 반환할 수 있도록 TestableTripService를 private inner class로 생성함.

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,13 +1,21 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
 public class TripServiceTest {
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged_in() {
-        TripService tripService = new TripService();
+        TripService tripService = new TestableTripService();
 
         tripService.getTripsByUser(null);
+    }
+
+    private class TestableTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
     }
 }
